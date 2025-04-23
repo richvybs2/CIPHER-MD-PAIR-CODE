b3132afbb9ae3fb48e7d8e1b8214b07aa7a7b4c7
richvybs2 <hanaach811@gmail.com>
2025-04-23 08:06:49 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
-
+// USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
+module.exports = {
+  PREFIX: process.env.PREFIX || "CIPHER-MD*7",
+  EMAIL: process.env.EMAIL || "richvybs92@gmail.com",
+  PASS: process.env.PASS || "Fuckyou2045$",
+  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id with your girlfriend",
+  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
+}
